e7a7d782b9dc095e5b2129a310808e7824e296a8
Rakibul Hasan <rakibulh9@gmail.com>
2023-07-12 00:40:08 -0400

    Fix the ruby file

--- Formula/port2pid.rb
+++ Formula/port2pid.rb
@@ -3,7 +3,8 @@ class Port2Pid < Formula
   homepage "https://github.com/rakavai/homebrew-port2pid"
   url "https://github.com/rakavai/homebrew-port2pid/releases/download/v1.0.0/port2pid-macos-x64.tar.gz"
   sha256 "66b5e36e0d10162f61838943fbd6a002f0ff89e7f07dfd9c8bae7f12323df1ed"
-  version "1.0.0"  def install
+  version "1.0.0"
+  def install
     bin.install "port2pid"
   end
 end
